28c87c2da9e18f47bdeb1a294edaf8327488c970
Imrancu <mdimran3772@gmail.com>
2021-07-02 19:18:19 +0600

    null vs undefined

---
+++ README.md
@@ -0,0 +1,1 @@
+# intermediete-js
---
+++ null-vs-undefined.js
@@ -0,0 +1,15 @@
+let pakhi
+
+function add(num1, num2) {
+    console.log(num1 + num2);
+}
+
+function add(num1, num2 = 10) {
+    console.log(num1, num2);
+}
+
+const premik = { name: 'Smart Girl', phone: 0343333509 }
+console.log(premik.gf);
+
+let fun = undefined
+console.log(fun);
