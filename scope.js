241d6f7e2313f657bb98e2530ecaf604ec170659
Imrancu <mdimran3772@gmail.com>
2021-07-03 00:18:39 +0600

    finished

---
+++ scope.js
@@ -0,0 +1,9 @@
+let bonus = 4
+
+function sum(first, second) {
+    let result = first + second + bonus
+    console.log(result);
+    return result
+}
+const output = sum(3, 8)
+console.log(output);
